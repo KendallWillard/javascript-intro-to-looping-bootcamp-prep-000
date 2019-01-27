794271bbf41ca95c145df34e7d8d2967847a78e9
kendall willard <kendallwillard@hotmail.com>
2019-01-27 22:45:44 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(alpha) {
+  for( var i = 0; i < 25; i++ ) {
+    if(i < 0 || i >= 25 ) {
+      alpha.push('I am ${i} strange loops.')
+    }
+    else {
+      alpha.push('I am ${i} strange loop.')
+    }
+  }
+}
