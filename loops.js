f346572cf3e6d51e911a38f1290f666dad9a2a76
kendall willard <kendallwillard@hotmail.com>
2019-01-27 22:50:38 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(alpha) {
   for( let i = 0; i < 25; i++ ) {
     if(i < 0 || i >= 25 ) {
-      alpha.push("I am " + i + "strange loops.")
+      alpha.push("I am ${i} strange loops.")
     }
     else {
       alpha.push("I am " + i  + " strange loop.")
